c16a9bf80dd8fb18d83e98a182e490a928887247
priti <priti12bishnoi@gmail.com>
2025-07-12 19:24:52 +0530

    cors error

--- index.js
+++ index.js
@@ -16,25 +16,25 @@ import productRoutes from "./routes/productRoutes.js";
 // Middleware Setup (IMPORTANT ORDER)
 // Configure CORS properly
 
-// const corsOptions = {
-//   origin:
-//     process.env.NODE_ENV === "production"
-//       ? ["https://storeuiii.vercel.app", "https://storeapies.vercel.app"]
-//       : ["http://localhost:5173", "http://localhost:5000"],
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
-//   exposedHeaders: ["*", "Authorization"],
-// };
+const corsOptions = {
+  origin:
+    process.env.NODE_ENV === "production"
+      ? ["https://storeuiii.vercel.app", "https://storeapies.vercel.app"]
+      : ["http://localhost:5173", "http://localhost:5000"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
+  exposedHeaders: ["*", "Authorization"],
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-app.use(
-  cors({
-    origin: "http://localhost:5173" || "https://storeuiii.vercel.app",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173" || "https://storeuiii.vercel.app",
+//     credentials: true,
+//   })
+// );
 
 app.use(express.json()); // For JSON bodies
 app.use(express.urlencoded({ extended: true })); // For form data
